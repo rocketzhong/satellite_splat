76f53ff377e1dcfb24604f9f8d68cf0dc724df7f
rocketzhong <zhongyale@mails.gdut.edu.cn>
2025-08-09 16:29:33 +0800

    feat: ns-train-debug.py

--- ns-train-debug.py
+++ ns-train-debug.py
@@ -1,3 +1,8 @@
+"""
+Copied from ns-train, which is used to debug in Pycharm
+python it with args should be the same as ns-train
+"""
+
 import re
 import sys
 from nerfstudio.scripts.train import entrypoint
